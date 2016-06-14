d7b6a3ace57e9e25fe151404b5b0fcdf612dc027
Joshua Woodward <jdw6359@rit.edu>
2016-06-14 16:08:51 -0400

    Add routes for static content and api endpoints

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 'use strict';
 
 var express = require('express');
-
 var app = express();
+var router = express.Router();
 
-app.use(express.static('public'));
+// Serve up static html content
+app.use('/', express.static('public'));
+
+// Define routes for api
+router.get('/todos', function(req, res) {
+	res.json({todos: []});
+})
+
+// TODO: Add a POST route to create new entries
+// TODO: Add a PUT route to update existing entries
+// TODO: Add a DELETE route to delete existing entries
+
+// Mount the api router to the application
+app.use('/api', router);
 
 app.listen(3000, function() {
 	console.log('The server is running on port 3000');
