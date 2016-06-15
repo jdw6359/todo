7bcd15c5b566fba505ec5737294802b7416de893
Joshua Woodward <jdw6359@rit.edu>
2016-06-15 16:28:14 -0400

    Write PUT todos method

--- src/api/index.js
+++ src/api/index.js
@@ -15,25 +15,39 @@ var Todo = require('../models/todo');
 router.get('/todos', function(req, res) {
     Todo.find({}, function(err, todos) {
         if(err) {
-            return res.status(500).json({message: err.message});
+            return res.status(500).json({error: err.message});
         }
         res.json({todos: todos});
     });
 });
 
-//POST /todos
+// POST /todos
 router.post('/todos', function(req, res) {
     var todo = req.body;
     Todo.create(todo, function(err, todo) {
         if(err) {
-            return res.status(500).json({message: err.message});
+            return res.status(500).json({error: err.message});
         }
         res.json({todo: todo, message: 'Todo created!'});
-    })
+    });
 })
 
+// PUT /todos/:id
+router.put('/todos/:id', function(req, res) {
+    var id = req.params.id;
+    var todo = req.body;
 
-// TODO: Add a PUT route to update existing entries
+    if(todo && todo._id !== id) {
+        return res.status(500).json({error: 'Ids do not match'});
+    }
+    Todo.findByIdAndUpdate(id, todo, {new: true}, function(err, todo) {
+        if(err) {
+            return res.status(500).json({error: err.message});
+        }
+        res.json({todo: todo, message: 'Todo created!'});
+    });
+});
+
 // TODO: Add a DELETE route to delete existing entries
 
 module.exports = router;
